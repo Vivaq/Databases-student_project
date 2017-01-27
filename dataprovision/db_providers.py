d4fe1287bbd816b78be07c57fcc9ba2250accd7c
Kamil <gryxon@gmail.com>
2017-01-27 11:08:41 +0100

    Adding autoincrement pks. Implementing db provider (6/8)

--- dataprovision/db_providers.py
+++ dataprovision/db_providers.py
@@ -26,10 +26,10 @@ class DbDataProvider(object):
         self.cur.execute(query)
         return self.cur.fetchall()
 
-    def add_eqs(self, eq_type_list):
-        for type in eq_type_list:
-            query = "insert into Urzadzenia values(sysdate," + type + ")"
-            self.cur.execute(query)
+    def add_eq(self, eq_type_id):
+        query = """INSERT INTO "KMAGRYTA"."URZADZENIE" (DATA_ZAKUPU, TYPURZADZENIA_IDTUR)
+                   VALUES (sysdate, """ + str(eq_type_id) + ")"
+        self.cur.execute(query)
 
     def get_doctors_with_type(self):
         query = """select p.pesel, p.nazwisko, t.specjalizacja from
@@ -39,7 +39,7 @@ class DbDataProvider(object):
         return self.cur.fetchall()
 
     def get_pref_hours_doctor(self, doctor_pesel):
-        query = """select dzien, TO_CHAR(\"start\", 'HH24:MI:SS'), TO_CHAR(koniec, 'HH24:MI:SS') from PrefGodzP
+        query = """select dzien, TO_CHAR(poczatek, 'HH24:MI:SS'), TO_CHAR(koniec, 'HH24:MI:SS') from PrefGodzP
                    where Pracownik_pesel = """ + str(doctor_pesel)
 
         self.cur.execute(query)
@@ -76,3 +76,4 @@ if __name__ == "__main__":
     print(provider.get_eq_providers_with_eq("Kardiogram"))
     print(provider.get_rooms_with_type())
     print(provider.get_type_eqs_by_time(3))
+    provider.add_eq(1)
